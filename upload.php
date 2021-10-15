fe1a5dd7270c240f4bbe42a0f34a9646bc9b67ec
Chiang <kithchayah.k@quo-global.com>
2021-10-15 17:44:59 +0700

    fixed return status , file type

--- upload.php
+++ upload.php
@@ -11,13 +11,17 @@ if ($_SESSION["token"]==$_POST["token"]) {
       $file_name = $_FILES['fileupload']['name'];
       $file_size =$_FILES['fileupload']['size'];
       $file_tmp =$_FILES['fileupload']['tmp_name'];
-      $file_type=$_FILES['fileupload']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['fileupload']['name'])));
+      
+      $file_type=explode('.',$file_name);
+      if(is_array($file_type)){
+         $file_type=end($file_type);
+      }
+      $file_ext=strtolower($file_type);
       
       $extensions= array("jpeg","jpg","png");
       
-      if(in_array($file_ext,$extensions)=== false){
-         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      if(in_array($file_ext,$extensions)=== false){         
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";         
       }
       
       if($file_size > 2097152){
@@ -28,6 +32,7 @@ if ($_SESSION["token"]==$_POST["token"]) {
          move_uploaded_file($file_tmp,"images/".$file_name);
          echo "Success";
       }else{
+         header('HTTP/1.0 403 Forbidden');
          print_r($errors);
       }
    }
